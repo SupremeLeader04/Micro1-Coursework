814c042bd2a24567cc93a0a0ef28edb3b6012c7a
Hayden Dyke <haydendyke1@gmail.com>
2025-02-12 21:23:11 +0000

    Dataset Preparation

--- Visualisations.py
+++ Visualisations.py
@@ -4,16 +4,17 @@ hours = pd.read_excel("./OECD_AnnualHours.xlsx")
 hours = hours.drop(columns=["Unnamed: 1", "Unnamed: 16"])
 hours = hours.fillna(0)
 hours.columns = hours.columns.astype(str)
-hours = pd.wide_to_long(hours, stubnames="20", j="Year", i="Country")
-hours = hours.rename({hours.columns[-1]: "Hours_Worked"})
-
+hours = hours.melt(id_vars=["Country"], var_name="Year", value_name="Working_Hours")
 
 strictness = pd.read_excel("./OECD_EmploymentStrictness.xlsx")
 strictness = strictness.fillna(0)
 strictness.columns = strictness.columns.astype(str)
-strictness = pd.wide_to_long(strictness, stubnames="20", j="Year", i="Country")
+strictness = strictness.melt(id_vars=["Country"], var_name="Year", value_name="Labour_Market_Strictness")
 
 wages = pd.read_excel("./OECD_WagesPPPDollar2023.xlsx")
 wages = wages.drop(columns=["Unnamed: 25"])
+wages = wages.fillna(0)
+wages.columns = wages.columns.astype(str)
+wages = wages.melt(id_vars=["Country"], var_name="Year", value_name="Average_Annual_Wage")
 
-print(hours.head())
+print(wages.head())
